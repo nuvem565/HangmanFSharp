37a34aa33dbf7ee8a1081d61bb2b6097f489cfa4
nuvem565 <tom.jedrzejewski565@gmail.com>
2020-11-21 22:01:14 +0100

    Filters the european countries
    Sets up the project, reads the lines of text file with countries with their capitals and filter only european ones.

--- Hangman/Program.fs
+++ Hangman/Program.fs
@@ -1,7 +1,78 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open System
+open System.IO
+
+module HangmanGame =
+
+    // Finds a text file in project directory
+    let countriesFileName = "countries_and_capitals.txt"
+    let sourceDirectory = __SOURCE_DIRECTORY__
+    let countriesWithCapital = File.ReadAllLines(sourceDirectory + "\\" + countriesFileName)
+
+    // list of european countries as in the C# version
+    let europeanCountries = [ 
+        "Albania";
+        "Andorra";
+        "Armenia";
+        "Austria";
+        "Belarus";
+        "Belgium";
+        "Bosnia and Herzegovina";
+        "Bulgaria";
+        "Croatia";
+        "Cyprus";
+        "Czech Republic";
+        "Denmark";
+        "Estonia";
+        "Finland";
+        "France";
+        "Germany";
+        "Greece";
+        "Hungary";
+        "Iceland";
+        "Ireland";
+        "Italy";
+        "Kosovo";
+        "Latvia";
+        "Liechtenstein";
+        "Lithuania";
+        "Luxembourg";
+        "Macedonia";
+        "Malta";
+        "Moldova";
+        "Monaco";
+        "Montenegro";
+        "The Netherlands";
+        "Norway";
+        "Poland";
+        "Portugal";
+        "Romania";
+        "Russia";
+        "San Marino";
+        "Serbia";
+        "Slovakia";
+        "Slovenia";
+        "Spain";
+        "Sweden";
+        "Switzerland";
+        "Ukraine";
+        "United Kingdom";
+        "Vatican City"]
+
+    let europeans = 
+        Array.toList countriesWithCapital
+        // filter for only european countries with respect to "europeanCountries" list
+        |> List.filter (
+            fun line -> 
+                let country = line.Substring(0, line.IndexOf('|')).Trim()
+                List.contains country europeanCountries) 
+        // Divide all string lines into a tuple
+        |> List.map (
+            fun pair ->
+                let arrOfTwo = pair.Split([|'|'|], 2)
+                arrOfTwo.[0].Trim() , arrOfTwo.[1].Trim()
+                )
+
+    printf "%A" europeans
